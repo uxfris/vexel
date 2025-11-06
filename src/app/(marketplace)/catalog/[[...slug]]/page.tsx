25d3d3b9a9f98e5a8fd14d100f8342941de1bb07
Fris El <uxfris@gmail.com>
2025-11-06 09:03:30 -0500

    feat: code refactoring

--- src/app/(marketplace)/catalog/[[...slug]]/page.tsx
+++ src/app/(marketplace)/catalog/[[...slug]]/page.tsx
@@ -1,17 +1,6 @@
-import { ChevronDown } from "lucide-react";
-import { Badge } from "@/components/ui/badge";
+import Header from "../components/Header";
 import PluginCard from "../components/PluginCard";
 
-const tags = [
-  {
-    id: "1",
-    name: "Animation",
-  },
-  {
-    id: "2",
-    name: "Text",
-  },
-];
 const mock = {
   featured: [
     {
@@ -108,44 +97,7 @@ export default async function CatalogPage({
   const data = await getItemsByCategory(slug);
   return (
     <>
-      <div className="h-15"></div>
-      <div className="text-md font-semibold flex items-center gap-2">
-        <span className="cursor-pointer">Vexel</span>→
-        <span className="cursor-pointer">Plugins</span>→
-        <span className="font-medium text-muted-foreground">
-          {data[0].category}
-        </span>
-      </div>
-      <h1 className="text-3xl md:text-5xl font-bold py-5">
-        {data[0].category}
-      </h1>
-      <p className="text-md font-medium text-muted-foreground pb-5">
-        Discover the best animation plugins for your projects.
-      </p>
-      <div className="wrap space-x-2">
-        {tags.map((tag) => (
-          <Badge
-            key={tag.id}
-            variant={"outline"}
-            className="shadow-xs py-2 px-4"
-          >
-            <p className="text-sm text-muted-foreground"> {tag.name} </p>
-          </Badge>
-        ))}
-      </div>
-      <div className="mt-10">
-        <div className="flex gap-4 items-center justify-between">
-          <div className="flex items-center gap-1">
-            <p className="text-2xl font-bold">Recent</p>
-            <ChevronDown className="w-6 h-6" />
-          </div>
-          <div className="flex items-center gap-1">
-            <p className="text-sm font-bold">Paid + Free</p>
-            <ChevronDown className="w-6 h-6" />
-          </div>
-        </div>
-        <div className="h-px border-b border-border mt-3 mb-6"></div>
-      </div>
+      <Header category={data[0]?.category || ""} />
       <PluginCard />
     </>
   );
