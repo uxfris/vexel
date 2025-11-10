efd86fc004730cb91b1213a9c38604afac43de8c
Fris El <uxfris@gmail.com>
2025-11-09 21:01:18 -0500

    fat: add not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,49 @@
+import Brand from "@/components/ui/brand";
+import { Button } from "@/components/ui/button";
+import Footer from "@/components/ui/footer";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+
 export default function notFound() {
   return (
-    <div className="text-center mt-20">
-      <h1 className="text-3xl font-bold">404 — Page Not Found</h1>
-      <p className="mt-4 text-gray-500">
-        Sorry, we couldn't find what you're looking for.
+    <div className="max-w-xl mx-auto mt-20 p-4">
+      <Brand className="w-8" />
+      <h1 className="text-2xl md:text-4xl font-bold my-4">
+        404 — Page Not Found
+      </h1>
+      <p className="text-base md:text-lg text-muted-foreground font-medium">
+        It seems like the page you were looking for has moved, changed, or
+        perhaps it was never here to begin with. No worries though, we can help
+        you navigate back!
       </p>
+      <div className="mt-12 flex flex-col gap-3">
+        <div className="border border-border rounded-sm p-4 font-semibold">
+          Try checking the URL for any errors, then hit refresh.
+        </div>
+        <div className="border border-border rounded-sm p-4 font-semibold">
+          Use the search bar at the top of the page to find what you were
+          looking for.
+        </div>
+        <div className="flex flex-col gap-4 border border-border rounded-sm p-4 font-semibold">
+          Or simply head back to our homepage
+          <Link href="/">
+            <Button
+              variant="outline"
+              className="flex gap-1 bg-muted font-bold text-foreground w-full shadow-sm"
+            >
+              Go to Vexel Homepage <ArrowRight className="w-5" />
+            </Button>
+          </Link>
+        </div>
+      </div>
+      <p className="md:text-lg font-medium text-muted-foreground mt-12">
+        Sorry for the detour. Happy browsing!
+      </p>
+      <Footer
+        isWithLetterSubscription={false}
+        isWithNavigation={false}
+        isMobileOnly={true}
+      />
     </div>
   );
 }
--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -5,8 +5,19 @@ import Divider from "./divider";
 import Image from "next/image";
 import { SiX } from "@icons-pack/react-simple-icons";
 import Brand from "./brand";
+import { cn } from "@/lib/utils/cn";
 
-export default function Footer() {
+type FooterProps = {
+  isWithLetterSubscription?: boolean;
+  isWithNavigation?: boolean;
+  isMobileOnly?: boolean;
+};
+
+export default function Footer({
+  isWithLetterSubscription = true,
+  isWithNavigation = true,
+  isMobileOnly = false,
+}: FooterProps) {
   const navLinks = {
     Resources: ["Overview", "Features", "Pricing", "Releases"],
     Marketplace: ["About", "Careers", "Press", "Contact"],
@@ -15,53 +26,63 @@ export default function Footer() {
     Socials: ["Privacy Policy", "Terms of Service", "Cookie Policy"],
   };
   return (
-    <footer className="w-full border-t border-border py-12">
-      <div className="flex flex-col md:flex-row items-start md:items-center justify-between gap-4 md:gap-12 w-full">
-        <div className="grow">
-          <h1 className="text-2xl md:text-3xl font-bold mb-2">
-            Vexel Newsletter
-          </h1>
-          <p className="text-muted-foreground font-medium">
-            Get the scoop on our newest releases, updates, and insightful
-            articles.
-          </p>
-        </div>
-        <div className="flex flex-col md:flex-row gap-2 border border-border p-4 md:p-6 w-full md:w-auto md:grow rounded-xl">
-          <Input
-            className="bg-input h-14 text-lg placeholder:text-muted-foreground-secondary"
-            placeholder="Your Email"
-          />
-          <Button
-            size="xl"
-            variant="outline"
-            className="w-full md:w-auto md:px-4 text-lg font-bold text-foreground"
-          >
-            Sign Up
-          </Button>
-        </div>
-      </div>
-      <div className="grid grid-cols-2 md:grid-cols-5 mt-5">
-        {Object.entries(navLinks).map(([section, links]) => (
-          <div key={section}>
-            <h3 className="mb-2 mt-6 text-sm font-bold text-muted-foreground-secondary">
-              {section}
-            </h3>
-            <ul className="space-y-2 text-sm">
-              {links.map((link) => (
-                <li key={link}>
-                  <Link
-                    href={`/${link.toLocaleLowerCase().replace("/\s+/g", "-")}`}
-                    className="font-medium hover:text-muted-foreground"
-                  >
-                    {link}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+    <footer
+      className={cn(
+        "w-full border-t border-border py-12",
+        isMobileOnly ? "md:hidden" : "",
+        isWithLetterSubscription ? "" : "border-none"
+      )}
+    >
+      {isWithLetterSubscription && (
+        <div className="flex flex-col md:flex-row items-start md:items-center justify-between gap-4 md:gap-12 w-full">
+          <div className="grow">
+            <h1 className="text-2xl md:text-3xl font-bold mb-2">
+              Vexel Newsletter
+            </h1>
+            <p className="text-muted-foreground font-medium">
+              Get the scoop on our newest releases, updates, and insightful
+              articles.
+            </p>
           </div>
-        ))}
-      </div>
-      <Divider classNames="mt-12 mb-4" />
+          <div className="flex flex-col md:flex-row gap-2 border border-border p-4 md:p-6 w-full md:w-auto md:grow rounded-xl">
+            <Input
+              className="bg-input h-14 text-lg placeholder:text-muted-foreground-secondary"
+              placeholder="Your Email"
+            />
+            <Button
+              size="xl"
+              variant="outline"
+              className="w-full md:w-auto md:px-4 text-lg font-bold text-foreground"
+            >
+              Sign Up
+            </Button>
+          </div>
+        </div>
+      )}
+      {isWithNavigation && (
+        <div className="grid grid-cols-2 md:grid-cols-5 mt-5">
+          {Object.entries(navLinks).map(([section, links]) => (
+            <div key={section}>
+              <h3 className="mb-2 mt-6 text-sm font-bold text-muted-foreground-secondary">
+                {section}
+              </h3>
+              <ul className="space-y-2 text-sm">
+                {links.map((link) => (
+                  <li key={link}>
+                    <Link
+                      href={`/${link.toLocaleLowerCase().replace("/\s+/g", "-")}`}
+                      className="font-medium hover:text-muted-foreground"
+                    >
+                      {link}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
+      {isWithNavigation && <Divider classNames="mt-12 mb-4" />}
       <div className="flex flex-col md:flex-row items-start md:items-center justify-between gap-3">
         <div className="flex items-center gap-2">
           <Brand className="w-5" />
